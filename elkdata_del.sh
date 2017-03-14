c6f9d62ff65ce10edf601209f7a5b9f8e5ea6656
root <root@Hexindai-BJ-DFGC-66.(none)>
2017-03-14 17:51:35 +0800

    update first shell

--- elkdata_del.sh
+++ elkdata_del.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -XGET http://172.16.10.66:9200/_cat/indices > /export/elk/elkindex.txt 2>/dev/null
+curl -XGET http://localhost:9200/_cat/indices > elkindex.txt 2>/dev/null
 
 DATA=`date --date='10 days ago' +%Y.%m.%d`
 
@@ -9,8 +9,8 @@ INDEX=`awk '{print $3}' /root/elkindex.txt`
 for i in $INDEX
 	do
 		if [[ $i =~ $DATA ]]; then
-			echo $i 'was deleted' >> /export/elk/elk_del_data.log 2>/dev/null
-			curl -XDELETE http://172.16.10.66:9200/$i >/dev/null 2>&1
+			echo $i 'was deleted' >> elk_del_data.log 2>/dev/null
+			curl -XDELETE http://localhost:9200/$i >/dev/null 2>&1
 		fi
 	done
 
